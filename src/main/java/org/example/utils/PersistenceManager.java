a5db8acb2c93b00e47ef074c80afdfb16393d36b
bouchraouchite <bouchra.ouchite@e-polytechnique.ma>
2024-03-08 10:53:33 +0100

    utils

---
+++ src/main/java/org/example/utils/PersistenceManager.java
@@ -0,0 +1,21 @@
+package org.example.utils;
+package utils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class PersistenceManager {
+    private static EntityManagerFactory emf;
+
+    public static EntityManager getEntityManager() {
+        if (emf == null) {
+            emf = Persistence.createEntityManagerFactory("yourPersistenceUnit");
+        }
+        return emf.createEntityManager();
+    }
+
+    public static void closeEntityManagerFactory() {
+        if (emf != null && emf.isOpen()) emf.close();
+    }
+}
